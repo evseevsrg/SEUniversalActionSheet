6dc106b4b506c0011befd645b5546e8f0cad6c13
Evseev Sergey <evseev.srg@gmail.com>
2014-12-20 22:24:53 +0300

    demo project added

---
+++ UniversalActionSheetDemo/UniversalActionSheetDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UniversalActionSheetDemo
+//
+//  Created by Evseev Sergey on 20/12/14.
+//  Copyright (c) 2014 Sergey Evseev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UniversalActionSheetDemo/UniversalActionSheetDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UniversalActionSheetDemo
+//
+//  Created by Evseev Sergey on 20/12/14.
+//  Copyright (c) 2014 Sergey Evseev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
